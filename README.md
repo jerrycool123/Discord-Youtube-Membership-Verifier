ea47ec3b256708c81084a216fc49c7e4d7754dba
jerrycool123 <jerrycool71534@gmail.com>
2022-01-08 14:37:19 +0800

    add https support

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 - Download Google OAuth 2.0 client credentials, rename it to `client_secret.json` and place it under `nodejs` directory.
 - Create `./nodejs/.env` and put environment variables into it. You can refer to `./nodejs/.env.defaults`.
+- Provide `key.pem` and `cert.pem` under `./nodejs/src/ssl` for HTTPS support.
 
 ## Install
 
--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv-defaults';
+import fs from 'fs';
+import https from 'https';
 import './discord/bot';
 
 import { getMemberOnlyVideoComments } from './discord/utils/youtube';
@@ -8,12 +10,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 8080
+const httpsCredentials = {
+  key: fs.readFileSync(__dirname + '/' + './ssl/key.pem'),
+  cert: fs.readFileSync(__dirname + '/' + './ssl/cert.pem')
+};
 
 app.get('/oauth2callback', async(req, res) => {
+  const { code, state } = req.query;
+  if (!code || !state) return res.status(404).send('');
   res.send("<script>window.close();</script>");
   await getMemberOnlyVideoComments(req.query);
 });
 
-app.listen(PORT, () => {
+https.createServer(httpsCredentials, app).listen(PORT, () => {
   console.log(`Server listening at port ${PORT}!`);
 });
